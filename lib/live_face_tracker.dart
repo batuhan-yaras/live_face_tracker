4410cdaad8d2c22bbd2c3ddb44c2ba6a33f65315
Batuhan <batuhanyaras@yahoo.com>
2026-02-07 17:39:14 +0300

    chore: Update package exports for new architecture

--- lib/live_face_tracker.dart
+++ lib/live_face_tracker.dart
@@ -2,5 +2,5 @@ library live_face_tracker;
 
 export 'src/ui/face_tracker_view.dart';
 export 'src/ui/face_tracker_controller.dart';
-export 'src/ui/face_capture_result.dart'; // <--- YENİ
+export 'src/ui/face_capture_result.dart';
 export 'src/ui/painters/face_painter.dart';
